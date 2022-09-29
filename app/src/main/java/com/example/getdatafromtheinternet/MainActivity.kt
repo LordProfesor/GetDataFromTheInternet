7578cfd177366668fe411f3f4a69fc02379218b1
brais <lordprofesor@gmail.com>
2022-09-29 12:47:28 +0200

    -Implementation of the model. -Implementation of the service.

--- app/src/main/java/com/example/getdatafromtheinternet/MainActivity.kt
+++ app/src/main/java/com/example/getdatafromtheinternet/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.getdatafromtheinternet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import com.example.getdatafromtheinternet.model.TheMovieDbService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import kotlin.concurrent.thread
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            thread {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("https://api.themoviedb.org/3/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+                val service = retrofit.create(TheMovieDbService::class.java)
+                val popularMovies = service.listPopularMovies(getString(R.string.api_key))
+                val body = popularMovies.execute().body()
+                if (body != null) {
+                    Log.i("MainActivity: ", "Movie count: ${body.results.size}")
+                }
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/getdatafromtheinternet/model/TheMovieDbService.kt
@@ -0,0 +1,11 @@
+package com.example.getdatafromtheinternet.model
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface TheMovieDbService {
+
+    @GET("movie/popular")
+    fun listPopularMovies(@Query("api_key") apiKey : String) : Call<MovieDbResult>
+}
